5e19ed255ceb78c7a9d6d072614aa55a0f367f9b
pavel <pavel.boiko@gmail.com>
2013-08-07 16:04:16 +0300

    calendar work process

--- js/calendar.js
+++ js/calendar.js
@@ -1,11 +1,17 @@
-var view_calendar = {
-	init: function () {
-
-	},
-
-	bind: function () {
-
-	}
+(function ($) {
 
 
-};
+	$.fn.calendarLine = function (method, id) {
+		if (methods[method])
+			return methods[method].apply(this, [id]);
+		else
+			return methods.start.apply(this, [method]);
+	};
+
+
+})(jQuery);
+
+
+$(document).ready(function () {
+	$('CalendarLine').calendarLine();
+});
