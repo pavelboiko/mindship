de37406b3df4bc2cfd8ae156782ae43cb7169d9f
pavel <pavel.boiko@gmail.com>
2013-08-08 09:25:00 +0300

    Calendar step 1

--- js/calendar.js
+++ js/calendar.js
@@ -1,17 +1,49 @@
 (function ($) {
 
+	$.fn.calendarLine = function (date) {
+		el = this;
 
-	$.fn.calendarLine = function (method, id) {
-		if (methods[method])
-			return methods[method].apply(this, [id]);
-		else
-			return methods.start.apply(this, [method]);
+		view.init();
+
 	};
+
+	var view = {
+		month_box: 3,
+
+		init: function () {
+//			this.clear();
+//			this.render();
+		},
+
+		render: function () {
+			var html = '';
+
+			for (var i = 0; i < this.month_box; i++) {
+				html += this.render_month();
+			}
+
+			el.find('.calendar').html(html);
+		},
+
+		render_month: function (count_days) {
+			var html = '';
+
+			for(var i = 1; i <= count_days; i++) {
+				html += '<li>' + i + '</li>';
+			}
+
+			return html;
+		},
+
+		clear: function () {
+			el.find('.calendar').html('');
+		}
+	}, el;
 
 
 })(jQuery);
 
 
 $(document).ready(function () {
-	$('CalendarLine').calendarLine();
+	$('#CalendarLine').calendarLine();
 });
