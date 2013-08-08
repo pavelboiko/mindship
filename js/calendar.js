1f07230a8eb908de335fc0a811b24bc60cfb32b2
pavel <pavel.boiko@gmail.com>
2013-08-08 19:39:52 +0300

    Calendar Scroll Happy End

--- js/calendar.js
+++ js/calendar.js
@@ -15,14 +15,22 @@
 			countMonthBox: 3,
 			curDate: {},
 			configLine: {
+				renderNewMonth: 20, // Add new Month if calendar < __% or > 100-__%
 				scrollSpeed: 1000,
 				scrollStep: 10,
 				viewPosCurrentDay: 8,
 				widthDay: 53 // width + border days box
 			},
+			dragging: null,
 
 
 			init: function (date) {
+				if (typeof preventSelection == 'function') {
+					preventSelection(document.getElementById('CalendarLine'));
+				} else {
+					console.log('Sory but don`t have function `preventSelection`!');
+				}
+
 				this.curDate = date; // Current day
 
 				this.clear()
@@ -70,7 +78,7 @@
 			},
 
 			renderMonth: function (objDate, countDays) {
-				var html = '<ul style="width: ' + (countDays * this.configLine.widthDay) + ';">' +
+				var html = '<ul data-time="' + objDate.getTime() + '" style="width: ' + (countDays * this.configLine.widthDay) + ';">' +
 						'<li class="month_line">' + monthsLong[objDate.getMonth()] + ' ' + objDate.getFullYear() + '</li>',
 					classToday = '',
 					isToday = objDate.getMonth() == this.curDate.getMonth() && objDate.getFullYear() == this.curDate.getFullYear(),
@@ -83,8 +91,8 @@
 					}
 					html += '<li' + classToday + '>' + i + '</li>';
 				}
-
 				html += '</ul>';
+
 				return html;
 			},
 
@@ -92,43 +100,94 @@
 				var self = this;
 				$('a', '#CalendarSwitcher').click(function () {
 					self.scrollButton($(this).hasClass('right'));
-
 					return false;
 				});
 
-				var dragging = null;
 				el.on('mousedown', 'li.month_line', function (e) {
-					dragging = e.pageX;
+					self.dragging = e.pageX;
 				}).on('mousemove', 'li', function (e) {
-					if (dragging) {
-						var size = dragging - e.pageX;
+					if (self.dragging) {
+						var size = self.dragging - e.pageX;
+						self.dragging = e.pageX;
 
-						self.scroll(Math.abs(size), ((size < 0) ? false : true), 0);
+						self.drag(Math.abs(size), ((size < 0) ? false : true), 0);
 
-						dragging = e.pageX;
+
 					}
-				}).on('mouseup', function (e) {
-					dragging = null;
+				}).on('mouseup', function () {
+					self.dragging = null;
 				});
+
+				return this;
 			},
 
 			scrollButton: function (direction) {
 				var size = this.configLine.scrollStep * this.configLine.widthDay;
-				console.log(size, direction);
 				this.scroll(size, direction);
 			},
 
-			scroll: function (size, direction, speed) {
-				if (typeof speed == 'undefined') {
-					speed = this.configLine.scrollSpeed;
-				}
-
-				el.animate({
+			scroll: function (size, direction) {
+				el.stop(true, true).animate({
 					left: (direction ? '-' : '+') + '=' + size + 'px'
-				}, speed, function () {
-					console.log('Ok');
-				});
+				}, this.configLine.scrollSpeed, this.corectPosition());
 			},
+
+			drag: function (size, direction) {
+				el.css({
+					left: (direction ? '-' : '+') + '=' + size + 'px'
+				});
+				this.corectPosition();
+			},
+
+			corectPosition: function () {
+				var min = el.width() * this.configLine.renderNewMonth/100,
+					max = el.width() - (el.parent().width()/2) - min,
+					left = Math.abs(parseFloat(el.css('left')));
+
+				if (min > left) {
+					this.prependMonth();
+				} else if (max < left)  {
+					this.appendMonth();
+				}
+			},
+
+			prependMonth: function () {
+				console.log('prependMonth');
+				var time = el.find('ul').first().data('time'),
+					ul_last = el.find('ul').last(),
+					nextMonth = new Date(time);
+
+				nextMonth.setMonth(nextMonth.getMonth() - 1);
+
+				var contMonthDays = new Date(nextMonth.getFullYear(), nextMonth.getMonth() + 1, 0).getDate();
+
+				el.css({
+					width: '+=' + ((contMonthDays * this.configLine.widthDay) - ul_last.width()),
+					left: '-=' + (contMonthDays * this.configLine.widthDay)
+				}).prepend(this.renderMonth(nextMonth, contMonthDays));
+
+				ul_last.remove();
+			},
+
+			appendMonth: function () {
+				console.log('appendMonth');
+				var time = el.find('ul').last().data('time'),
+					ul_first = el.find('ul').first(),
+					nextMonth = new Date(time);
+
+				nextMonth.setMonth(nextMonth.getMonth() + 1);
+
+				var contMonthDays = new Date(nextMonth.getFullYear(), nextMonth.getMonth() + 1, 0).getDate(),
+					s = (contMonthDays * this.configLine.widthDay) - ul_first.width();
+
+				el.css({
+					width: '+=' + s,
+					left: '+=' + ul_first.width()
+				}).append(this.renderMonth(nextMonth, contMonthDays));
+
+				ul_first.remove();
+			},
+
 
 			loading: function (type) {
 
