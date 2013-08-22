20dc3cf618f5843e75a231225b3e43f6b4fc66df
AlexKorop <bjrockrider@gmail.com>
2013-08-22 13:17:18 +0300

    -array

--- server/tasks.php
+++ server/tasks.php
@@ -1,7 +1,6 @@
 <?php
 $data = array();
 $data['1375304400000'][] = array('dateStart' => '15', 'dateEnd' => '24', 'length' => '11' );
-$data['1375304400000'][] = array('dateStart' => '25', 'dateEnd' => '29', 'length' => '5' );
 echo $_GET['callback']. "(".json_encode($data).");";
 ?>
 
