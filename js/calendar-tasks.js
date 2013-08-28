f08d9e546509f5a284b75feb2daf17ef662d1d36
pavel <pavel.boiko@gmail.com>
2013-08-28 16:32:59 +0300

    calendar test go!

--- js/calendar-tasks.js
+++ js/calendar-tasks.js
@@ -1,113 +1,4 @@
-var objTasks = {
-	init: function () {
-		this.bind();
-		return this;
-	},
-
-	pushAllData: function (times) {
-		var self = this;
-		$.ajax({
-			type: "GET",
-			url: "server/tags.php",
-			data: { t: times.join(','), s: this.s },
-			dataType: "json",
-			success: function (res) {
-				self.el.find('ul').each(function () {
-					var ul = $(this);
-					self.push(res, ul);
-				});
-			}
-		});
-
-		return this;
-	},
-
-	pushData: function (el) {
-		var self = this;
-
-		$.ajax({
-			type: "GET",
-			url: "server/tags.php",
-			data: {t: el.data('time'), s: ''},
-			dataType: "json",
-			success: function (res) {
-				self.push(res, el);
-			}
-		});
-	},
-
-	bind: function () {
-
-	},
-
-	push: function (res, el) {
-		this.step = 1;
-		var time = +el.data('time');
-
-		if (typeof res[time] == 'undefined') {
-			return;
-		}
-
-		var models = res[el.data('time')];
-
-		for (var i = 0, c = models.length; i < c; i++) {
-			el.append(this.templates(models[i], time));
-		}
-	},
-
-	step: 1,
-	templates: function (model, time) {
-
-		var widthDay = this.parent.configLine.widthDay,
-			start = (time - +model[2]) / (3600 * 24 * 1000),
-			end = (+model[3] - time) / (3600 * 24 * 1000),
-			css = {};
-
-		console.log(model[0], new Date(+model[2]), new Date(+model[3]), start, end, model[6], model[7]);
-
-		css = {
-			top: (this.step * 25 + 25) + 'px',
-			left: (start * widthDay) + 'px',
-			width: (end * widthDay) - 10 + 'px'
-		};
-
-
-		if ($('#task_' + model[0]).length) {
-			css['top'] = $('#task_' + model[0]).css('top');
-			css['left'] = $('#task_' + model[0]).css('left');
-			css['width'] = $('#task_' + model[0]).css('width');
-		}
-
-		$('#task_' + model[0]).remove();
-
-		this.step++;
-
-		return $('<li id="task_' + model[0] + '" class="task">' + model[1] + ' DS:' + this.gDate(model[2]) + ' DF:' + this.gDate(model[3]) + ' DC:' + this.gDate(time) + '</li>').css(css);
-	},
-
-	gDate: function (time) {
-		var date = new Date();
-
-		date.setTime(time);
-		return date.getDay() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
-	}
-//	setLine: function () {
-//		var el = $('CalendarLine').find('.calendar'),
-//			models = {};
-//
-//		el.find('ul').each(function () {
-//			models[$(this).data('time')] = $(this).find('li');
-//		});
-//
-////		for (var i in this.data) {
-////			models[] = {};
-////		}
-//	}
-
-};
-
 function Tasks() {
-
 	var root_el = {},
 		s_val = '',
 		tasks = [],
@@ -134,9 +25,47 @@ function Tasks() {
 	}
 
 	function getWidthByTime(time) {
-		return (53 * (time - 1) / (3600 * 24 * 1000)).toFixed(2);
+		return 53 * Math.round((time - 1) / (3600 * 24 * 1000));
 	}
 
+	function getCalendarSize() {
+		var left = Math.abs(parseFloat(root_el.css('left'))),
+			root_width = root_el.parent().width(),
+			calendar_width = root_el.width();
+
+		return [left, +left + +root_width];
+	}
+
+	function siftTasks(tasks) {
+		var new_tasks = [],
+			last_line = root_el.find('ul:last'),
+			all_line_size = [+root_el.find('ul:first').data('time'), +last_line.data('time') + (last_line.find('li').length - 1) * 24 * 3600 * 1000],
+			active_line_size = getCalendarSize();
+
+		position = 0;
+
+		for (var i = 0, j = tasks.length; i < j; i++) {
+
+			var task_left = tasks[i].left + tasks[i].width;
+
+			if (tasks[i].start > all_line_size[1] || tasks[i].finish < all_line_size[0]) {
+				console.log(tasks[i].id, all_line_size, tasks[i].start, tasks[i].finish, tasks[i].start > all_line_size[1], tasks[i].finish < all_line_size[0]);
+				continue;
+			}
+
+			if (tasks[i].left < active_line_size[1] && task_left > active_line_size[0]) {
+				tasks[i].position = position;
+
+				position++;
+			} else {
+				tasks[i].position = 0;
+			}
+
+			new_tasks.push(tasks[i]);
+		}
+
+		return new_tasks;
+	}
 
 	function pushEvents(res) {
 		var start_time = root_el.find('ul:first').data('time'),
@@ -150,12 +79,14 @@ function Tasks() {
 				finish: +res[i][3],
 				sd: new Date(+res[i][2]),
 				fd: new Date(+res[i][3]),
-				left: getWidthByTime(start_time - res[i][2]), //53 * (start_time - res[i][2]) / (3600 * 24 * 1000),
+				left: -getWidthByTime(start_time - res[i][2]), //53 * (start_time - res[i][2]) / (3600 * 24 * 1000),
 				width: getWidthByTime(res[i][3] - res[i][2] + (25 * 3600)), //53 * (res[i][3] - start_time) / (3600 * 24 * 1000),
 				position: position++,
 				today: getWidthByTime(+res[i][3] - today.getTime())
 			});
 		}
+		tasks = siftTasks(tasks);
+		console.log(tasks);
 
 		for (var i = tasks.length - 1; i > 0; i--) {
 			if (tasks[i] === undefined) {
@@ -167,16 +98,23 @@ function Tasks() {
 
 			if (!$('#' + id).length) {
 
-				var before = '',
-					info = '';
-				console.log(tasks[i].today);
+				var before_stile = '',
+					info = tasks[i].name + ' s=' + tasks[i].sd + ' f=' + tasks[i].fd,
+					task_style = 'style="width:' + tasks[i].width + 'px;margin-left:' + tasks[i].left + 'px;';
+
 				if (tasks[i].today > 0) {
-					before = ' style="width: ' + tasks[i].today + 'px"';
+					before_stile = ' style="width: ' + tasks[i].today + 'px"';
 				}
-				info = tasks[i].name + ' s=' + tasks[i].sd + ' f=' + tasks[i].fd;
 
+				if (tasks[i].position) {
+					console.log(tasks[i]);
+					task_style += 'display: block;';
+				}
 
-				root_el.find('.new_task_bar').append('<div id="' + id + '" data-array=" ' + i + '" class="current_task" style="width:' + tasks[i].width + 'px;margin-left:' + -tasks[i].left + 'px;"><span>' + info + '</span><div' + before + ' class="over_today"></div></div>');
+				task_style += '"';
+
+				root_el.find('.new_task_bar').append('<div id="' + id + '" data-array=" ' + i + '" class="current_task"' + task_style + '><span>' + info + '</span><div' + before_stile + ' class="over_today"></div></div>');
+			} else {
 			}
 		}
 
@@ -192,7 +130,6 @@ function Tasks() {
 	}
 
 
-
 	this.init = function () {
 		console.log('---====_Start_====---');
 
@@ -201,9 +138,9 @@ function Tasks() {
 
 		root_el.on('mousedown', '.current_task', function (e) {
 			var drag = {
-				el: $(this),
-				move: e.pageX
-			},
+					el: $(this),
+					move: e.pageX
+				},
 				array_id = +drag.el.data('array');
 
 			if (tasks[array_id] === undefined) {
@@ -212,7 +149,7 @@ function Tasks() {
 			}
 
 			$(document).mousemove(function (e) {
-				var size = parseInt((drag.move - e.pageX)/53);
+				var size = parseInt((drag.move - e.pageX) / 53);
 
 
 				if (size != 0) {
@@ -236,13 +173,7 @@ function Tasks() {
 		});
 
 
-
-
 		return this;
-	};
-
-	this.dragTask = function (size) {
-
 	};
 
 	this.changeSearch = function (s) {
---
+++ js/odi_tasks.js
@@ -0,0 +1,107 @@
+var objTasks = {
+	init: function () {
+		this.bind();
+		return this;
+	},
+
+	pushAllData: function (times) {
+		var self = this;
+		$.ajax({
+			type: "GET",
+			url: "server/tags.php",
+			data: { t: times.join(','), s: this.s },
+			dataType: "json",
+			success: function (res) {
+				self.el.find('ul').each(function () {
+					var ul = $(this);
+					self.push(res, ul);
+				});
+			}
+		});
+
+		return this;
+	},
+
+	pushData: function (el) {
+		var self = this;
+
+		$.ajax({
+			type: "GET",
+			url: "server/tags.php",
+			data: {t: el.data('time'), s: ''},
+			dataType: "json",
+			success: function (res) {
+				self.push(res, el);
+			}
+		});
+	},
+
+	bind: function () {
+
+	},
+
+	push: function (res, el) {
+		this.step = 1;
+		var time = +el.data('time');
+
+		if (typeof res[time] == 'undefined') {
+			return;
+		}
+
+		var models = res[el.data('time')];
+
+		for (var i = 0, c = models.length; i < c; i++) {
+			el.append(this.templates(models[i], time));
+		}
+	},
+
+	step: 1,
+	templates: function (model, time) {
+
+		var widthDay = this.parent.configLine.widthDay,
+			start = (time - +model[2]) / (3600 * 24 * 1000),
+			end = (+model[3] - time) / (3600 * 24 * 1000),
+			css = {};
+
+		console.log(model[0], new Date(+model[2]), new Date(+model[3]), start, end, model[6], model[7]);
+
+		css = {
+			top: (this.step * 25 + 25) + 'px',
+			left: (start * widthDay) + 'px',
+			width: (end * widthDay) - 10 + 'px'
+		};
+
+
+		if ($('#task_' + model[0]).length) {
+			css['top'] = $('#task_' + model[0]).css('top');
+			css['left'] = $('#task_' + model[0]).css('left');
+			css['width'] = $('#task_' + model[0]).css('width');
+		}
+
+		$('#task_' + model[0]).remove();
+
+		this.step++;
+
+		return $('<li id="task_' + model[0] + '" class="task">' + model[1] + ' DS:' + this.gDate(model[2]) + ' DF:' + this.gDate(model[3]) + ' DC:' + this.gDate(time) + '</li>').css(css);
+	},
+
+	gDate: function (time) {
+		var date = new Date();
+
+		date.setTime(time);
+		return date.getDay() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
+	}
+//	setLine: function () {
+//		var el = $('CalendarLine').find('.calendar'),
+//			models = {};
+//
+//		el.find('ul').each(function () {
+//			models[$(this).data('time')] = $(this).find('li');
+//		});
+//
+////		for (var i in this.data) {
+////			models[] = {};
+////		}
+//	}
+
+};
