4b3dda73637f6933051b2da4d467dae53533cc24
pavel <pavel.boiko@gmail.com>
2013-08-28 18:59:02 +0300

    go change mini!!!

--- js/calendar-tasks.js
+++ js/calendar-tasks.js
@@ -36,68 +36,76 @@ function Tasks() {
 		return [left, +left + +root_width];
 	}
 
-	function siftTasks(tasks) {
+	function siftTasks() {
 		var new_tasks = [],
-			last_line = root_el.find('ul:last'),
-			all_line_size = [+root_el.find('ul:first').data('time'), +last_line.data('time') + (last_line.find('li').length - 1) * 24 * 3600 * 1000],
+			key_array = [],
+			finish_ul = root_el.find('ul:last'),
+
+			start_time = +root_el.find('ul:first').data('time'),
+			finish_time = +finish_ul.data('time') + (finish_ul.find('li').length - 1) * 24 * 3600 * 1000,
+
 			active_line_size = getCalendarSize();
 
-		position = 0;
-
 		for (var i = 0, j = tasks.length; i < j; i++) {
-
-			var task_left = tasks[i].left + tasks[i].width;
-
-			if (tasks[i].start > all_line_size[1] || tasks[i].finish < all_line_size[0]) {
-				console.log(tasks[i].id, all_line_size, tasks[i].start, tasks[i].finish, tasks[i].start > all_line_size[1], tasks[i].finish < all_line_size[0]);
+			if (key_array.indexOf(tasks[i].id) >= 0 || tasks[i].start > finish_time || tasks[i].finish < start_time) {
+				if ($('#' + tasks[i].id).length) {
+					console.log($('#' + tasks[i].id));
+					$('#' + tasks[i].id).remove();
+				}
 				continue;
 			}
 
-			if (tasks[i].left < active_line_size[1] && task_left > active_line_size[0]) {
-				tasks[i].position = position;
+			tasks[i].width = getWidthByTime(tasks[i].finish - tasks[i].start);
+			tasks[i].left = -getWidthByTime(start_time - tasks[i].start);
+			tasks[i].today = getWidthByTime(tasks[i].finish - today.getTime());
 
-				position++;
-			} else {
-				tasks[i].position = 0;
-			}
+			var task_left = tasks[i].left + tasks[i].width;
+
+//			if ((tasks[i].left < active_line_size[1] && task_left > active_line_size[0])) {
+//				console.log(tasks[i].id);
+//			}
+			tasks[i].position = (tasks[i].left < active_line_size[1] && task_left > active_line_size[0]) ? 1 : 0;
 
 			new_tasks.push(tasks[i]);
+			key_array.push(tasks[i].id); // For test
 		}
 
-		return new_tasks;
+		tasks = new_tasks;
+	}
+
+	function intefaceTask (json_arr) {
+		return {
+			id: json_arr[0],
+			name: json_arr[1],
+			start: +json_arr[2],
+			finish: +json_arr[3],
+			sd: new Date(+json_arr[2]), // For Test
+			fd: new Date(+json_arr[3]), // For Test
+			left: 0,
+			width: 0,
+			today: 0,
+			position: 0
+		}
 	}
 
 	function pushEvents(res) {
 		var start_time = root_el.find('ul:first').data('time'),
 			start_date = new Date(start_time);
 
-		for (var i in res) {
-			tasks.push({
-				id: res[i][0],
-				name: res[i][1],
-				start: +res[i][2],
-				finish: +res[i][3],
-				sd: new Date(+res[i][2]),
-				fd: new Date(+res[i][3]),
-				left: -getWidthByTime(start_time - res[i][2]), //53 * (start_time - res[i][2]) / (3600 * 24 * 1000),
-				width: getWidthByTime(res[i][3] - res[i][2] + (25 * 3600)), //53 * (res[i][3] - start_time) / (3600 * 24 * 1000),
-				position: position++,
-				today: getWidthByTime(+res[i][3] - today.getTime())
-			});
+		for (var k in res) {
+			tasks.push(intefaceTask(res[k]));
 		}
-		tasks = siftTasks(tasks);
-		console.log(tasks);
+
+		siftTasks();
 
 		for (var i = tasks.length - 1; i > 0; i--) {
 			if (tasks[i] === undefined) {
-				console.log(i);
 				continue;
 			}
 
 			var id = 't_' + tasks[i].id;
 
 			if (!$('#' + id).length) {
-
 				var before_stile = '',
 					info = tasks[i].name + ' s=' + tasks[i].sd + ' f=' + tasks[i].fd,
 					task_style = 'style="width:' + tasks[i].width + 'px;margin-left:' + tasks[i].left + 'px;';
@@ -115,6 +123,17 @@ function Tasks() {
 
 				root_el.find('.new_task_bar').append('<div id="' + id + '" data-array=" ' + i + '" class="current_task"' + task_style + '><span>' + info + '</span><div' + before_stile + ' class="over_today"></div></div>');
 			} else {
+				if (tasks[i].position) {
+					console.log(tasks[i]);
+				}
+
+				$('#' + id).css({
+					width: tasks[i].width,
+					'margin-left': tasks[i].left,
+					display: (tasks[i].position) ? 'block' : 'hide'
+				}).find('.over_today').css({
+					width: (tasks[i].today > 0) ? tasks[i].today : 0
+				});
 			}
 		}
 
